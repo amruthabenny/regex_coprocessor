44d499042459cedf7d61b61c02236ef758a1fc4b
amruthabenny <112004003@smail.iitpkd.ac.in>
2023-05-02 16:41:10 +0530

    added example.py

--- re2compiler/example.py
+++ re2compiler/example.py
@@ -1,5 +1,16 @@
 import re2compiler
+import sys
+import os
 
-data 	= '(a|a_b)*'
-output	= re2compiler.compile(data=data)
-print(output)
+
+fp = None
+input_re = sys.argv[1]
+if os.path.isfile(input_re):
+    fp = open(input_re, "r")
+    regex = fp.readline()
+    #print(regex)
+    machine_code = re2compiler.compile(data=regex)
+    print("Resultant code:\n",machine_code)
+else:
+    print(f"{input_re} does not exist")
+    sys.exit(1)
